7dc4098d123dc0109a0fcf883ef3e41219085b87
zjhe <hezijie@microsoft.com>
2022-11-03 16:24:42 +0800

    fix make install with TFLINT_PLUGIN_DIR env set

--- install/main.go
+++ install/main.go
@@ -136,7 +136,11 @@ func install() {
 		baseDir := os.Getenv("USERPROFILE")
 		outputDir = fmt.Sprintf(`%s\.tflint.d\plugins`, baseDir)
 	}
-	_ = os.MkdirAll(outputDir, os.ModePerm)
+	if dir := os.Getenv("TFLINT_PLUGIN_DIR"); dir != "" {
+		outputDir = dir
+	} else {
+		_ = os.MkdirAll(outputDir, os.ModePerm)
+	}
 	//#nosec G204
 	cmd := exec.Command("go", "build", "-o", outputDir)
 	if err := cmd.Run(); err != nil {
