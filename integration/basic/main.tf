e1b432fdc5e762711029d584d8e5aef1cb748242
hezijie <lonegunmanb@hotmail.com>
2024-03-12 16:24:15 +0800

    fix incorrect integration test tf config

--- integration/basic/main.tf
+++ integration/basic/main.tf
@@ -1,34 +1,33 @@
 resource "azurerm_container_group" "example" {
-  name                = "example-continst"
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  ip_address_type     = "Public"
-  dns_name_label      = "aci-label"
+  name                = "example-continst"
   os_type             = "Linux"
-  depends_on          = [
-    azurerm_resource_group.example
-  ]
-
-  container {
-    name   = "hello-world"
-    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-          port     = 443
-          protocol = "TCP"
-    }
-  }
-
-  container {
-    name   = "sidecar"
-    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
-    cpu    = "0.5"
-    memory = "1.5"
-  }
-
+  resource_group_name = azurerm_resource_group.example.name
+  dns_name_label      = "aci-label"
+  ip_address_type     = "Public"
   tags = {
     environment = "testing"
   }
+
+  container {
+    cpu    = "0.5"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    memory = "1.5"
+    name   = "hello-world"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+  container {
+    cpu    = "0.5"
+    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
+    memory = "1.5"
+    name   = "sidecar"
+  }
+
+  depends_on = [
+    azurerm_resource_group.example
+  ]
 }
